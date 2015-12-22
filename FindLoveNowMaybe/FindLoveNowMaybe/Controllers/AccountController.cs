a01521e3cbd958de247807f7b248226637240749
kimdah3 <dahlberg.kim20@gmail.com>
2015-12-22 15:46:00 +0100

    Ändrat struktur. Account har actions for Login och Register. Inte skapat Login än.
    Ändrat struktur. Account har actions for Login och Register. Inte skapat
    Login än.

---
+++ FindLoveNowMaybe/FindLoveNowMaybe/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using FindLoveNowMaybe.Models;
+using Repositories;
+
+namespace FindLoveNowMaybe.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        // GET: Login
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return View(new LoginModel());
+        }
+
+        [HttpGet]
+        public ActionResult Index(LoginModel model)
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View(new RegistrationModel());
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegistrationModel model)
+        {
+            if (!ModelState.IsValid) return View(); //Om felaktig input, returnera tom view
+
+            var newUser = new User()
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                UserName = model.UserName,
+                Password = model.Password,
+                Description = model.Description,
+                Age = model.Age,
+                Picture = model.Picture,
+                Sex = model.Sex,
+                InterestedMen = model.InterestedMen,
+                InterestedWomen = model.InterestedWomen
+            };
+
+            using (var userRepository = new UserRepository())
+            {
+                userRepository.Context.User.Add(newUser);
+                userRepository.Save();
+            }
+            FormsAuthentication.SetAuthCookie(model.UserName, false);
+            return RedirectToAction("Index", "Account");
+        }
+    }
+}
--- FindLoveNowMaybe/FindLoveNowMaybe/Controllers/HomeController.cs
+++ FindLoveNowMaybe/FindLoveNowMaybe/Controllers/HomeController.cs
@@ -14,69 +14,18 @@ namespace FindLoveNowMaybe.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            using (var repostiry = new UserRepository())
-            {
-                var user = repostiry.GetFirstUser();
-                var user2 = repostiry.GetUser(2);
-                user.SentFriendRequest.Add(user2);
-                repostiry.Save();
-            }
-
-            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Register()
-        {
-            return View(new RegistrationModel());
-        }
-
-        [HttpPost]
-        public ActionResult Register(RegistrationModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var newUser = new User()
-                {
-                    FirstName = model.FirstName,
-                    LastName = model.LastName,
-                    UserName = model.UserName,
-                    Password = model.Password,
-                    Description = model.Description,
-                    Age = model.Age,
-                    Picture = model.Picture,
-                    Sex = model.Sex,
-                    InterestedMen = model.InterestedMen,
-                    InterestedWomen = model.InterestedWomen
-                };
-
-                using (var userRepository = new UserRepository())
-                {
-                    userRepository.Context.User.Add(newUser);
-                    userRepository.Save();
-                }
-                FormsAuthentication.SetAuthCookie(model.UserName, false);
-                return RedirectToAction("Login", "Home");
-            }
-            else
-            {
-                return View();
-            }
-            //var registrationCompleted = true; // Försök registrera...
-            //if (!registrationCompleted)
+            //******************
+            //KOD SOM TESTAR uTIFALL DET GÅR ATT SKICKA FRIENDREQUEST.
+            //******************
+            //using (var repostiry = new UserRepository())
             //{
-            //    ModelState.AddModelError("", "Ett felmeddelande...");
-            //    return View();
+            //    var user = repostiry.GetFirstUser();
+            //    var user2 = repostiry.GetUser(2);
+            //    user.SentFriendRequest.Add(user2);
+            //    repostiry.Save();
             //}
 
-            //FormsAuthentication.SetAuthCookie(model.UserName, false);
-            //return RedirectToAction("Index", "User");
-        }
-
-        public ActionResult Login()
-        {
             return View();
         }
-
     }
 }
