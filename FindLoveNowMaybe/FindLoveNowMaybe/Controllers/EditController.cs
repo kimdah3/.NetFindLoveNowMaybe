d2d36af4b48ea974bf499baa4d1d21b0ced52030
Christoffer Sibring <sibring92@gmail.com>
2015-12-29 19:37:10 +0100

    Har fixat så profil bilder används mot databasen Och lagt till model för EditUser och en View gick åt helvete när jag försökte uppdatera en användare så tog bort all kod !

---
+++ FindLoveNowMaybe/FindLoveNowMaybe/Controllers/EditController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FindLoveNowMaybe.Models;
+using Repositories;
+
+namespace FindLoveNowMaybe.Controllers
+{
+
+    [Authorize]
+    public class EditController : Controller
+    {
+        // GET: Edit
+        public ActionResult EditUser()
+        {
+            
+            return View();
+        }
+
+
+
+    }
+}
+
