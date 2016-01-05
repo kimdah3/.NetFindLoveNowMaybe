7a4afcb365175972b37436c95e2c715ba3223e7c
Christoffer Sibring <sibring92@gmail.com>
2016-01-05 15:22:44 +0100

    Fixat freindrequest metoder och börjat på edituser

--- FindLoveNowMaybe/FindLoveNowMaybe/Controllers/EditController.cs
+++ FindLoveNowMaybe/FindLoveNowMaybe/Controllers/EditController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using FindLoveNowMaybe.Models;
 using Repositories;
+using System.Web.Security;
 
 namespace FindLoveNowMaybe.Controllers
 {
@@ -12,17 +13,46 @@ namespace FindLoveNowMaybe.Controllers
     [Authorize]
     public class EditController : Controller
     {
-        // GET: Edit
-        public ActionResult EditUser(string userName)
+        [HttpGet]
+        public ActionResult EditUser()
         {
-            var usermodel = new UserRepository();
-            usermodel.GetUserByUserName(userName);
-            return View(userName);
+            return View(new EditModel());
         }
 
+        [HttpPost]           
+       
+            public ActionResult EditUser(EditModel model)
+        {
+            if (!ModelState.IsValid) return View(); //Om felaktig input, returnera view
 
-     
 
+            var activeUser = new User();
+
+            activeUser.FirstName = model.FirstName;
+            activeUser.LastName = model.LastName;
+            activeUser.UserName = model.UserName;
+            activeUser.Password = model.Password;
+            activeUser.Description = model.Description;
+            activeUser.Age = model.Age;
+            activeUser.Sex = model.Sex;
+            activeUser.InterestedMen = model.InterestedMen;
+            activeUser.InterestedWomen = model.InterestedWomen;
+            
+
+            using (var userRepository = new UserRepository())
+            {
+                if (!userRepository.IsUniqueUserName(activeUser.UserName))
+                {
+                    ModelState.AddModelError("", "Username already exists!");
+                    return View();
+                }
+                var editRepository = new EditRepository();
+                editRepository.UpdatePerson(model, activeUser);
+                userRepository.Save();
+            }
+            FormsAuthentication.SetAuthCookie(model.UserName, false);
+            return RedirectToAction("Index", "Account");
+        }
     }
 }
 
--- FindLoveNowMaybe/Repositories/FriendReqRepository.cs
+++ FindLoveNowMaybe/Repositories/FriendReqRepository.cs
@@ -1,22 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Repositories
-{/*
-    public static void SendFriendRequest(User sender, User Reciever)
+{
+    public class FriendReqRepository
     {
-        Reciever.SentFriendRequest.Add(sender);
-        sender.
-        var friendreq = new Friendrequest();
-        friendReq.FRequestSenderId = sender.ID;
-        friendReq.FRequestRecieverId = receiver.ID;
-        using (var db = new FindLoveDbEntities())
+        public static Boolean CheckActiveFriendRequests(User ActiveUser)
         {
-            db.FriendRequest.Add(friendReq);
-            db.SaveChanges();
+            var friendList = new List<Friend>();
+            using (var db = new FindLoveDbEntities())
+            {
+                var result = from r in db.Friend
+                    where r.SenderId == ActiveUser.Id && r.Accepted == false
+                    select r;
+                foreach (var item in result)
+                {
+                    friendList.Add(item);
+                }
+                var returnboolean = false ;
+
+                if (friendList.Count > 0)
+                {
+                    returnboolean = true;
+                }
+                return returnboolean;
+            }
+
         }
-    }*/
+
+        public static List<User> returnAllPendingRequestUsers(User ActiveUser)
+        {
+            var returnList = new List<User>();
+            using (var db = new FindLoveDbEntities())
+            {
+                var result = from r in db.Friend
+                    where r.ReceiverId == ActiveUser.Id && r.Accepted == false
+                    select r;
+                var UserRep = new UserRepository();
+                foreach (var item in result)
+                {
+                    returnList.Add(UserRep.GetUserById(item.SenderId));
+                }
+            }
+            return returnList;
+        }
+
+        public static void SendFriendRequest(User sender, User Reciever)
+        {
+
+            var friendReq = new Friend();
+            friendReq.SenderId = sender.Id;
+            friendReq.ReceiverId = Reciever.Id;
+            friendReq.Accepted = false;
+            using (var db = new FindLoveDbEntities())
+            {
+                db.Friend.Add(friendReq);
+                db.SaveChanges();
+            }
+        }
+    }
 }
+
+
