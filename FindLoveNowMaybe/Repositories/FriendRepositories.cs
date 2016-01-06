956288bf9a26e2ba4a26ddcd08b4c45fdd03b713
LinusIvarsson <ivarsson.linus@gmail.com>
2016-01-06 12:09:36 +0100

    Skapat klass för Friends. Skapat funktion som returnerar användarens alla vänner.

---
+++ FindLoveNowMaybe/Repositories/FriendRepositories.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    class FriendRepositories
+    {
+
+        public static List<User> ReturnAllFriends(User ActiveUser)
+        {
+            var returnList = new List<User>();
+
+            using (var db = new FindLoveDbEntities())
+            {
+                var result = from r in db.Friend
+                             where r.ReceiverId == ActiveUser.Id || r.SenderId == ActiveUser.Id && r.Accepted == true
+                             select r;
+                var UserRep = new UserRepository();
+                foreach(var item in result)
+                {
+                    if(item.ReceiverId == ActiveUser.Id)
+                    {
+                        returnList.Add(UserRep.GetUserById(item.ReceiverId));
+                    }
+                    else
+                    {
+                        returnList.Add(UserRep.GetUserById(item.SenderId));
+                    }
+                }
+            }
+            return returnList;
+
+        }
+
+
+    }
+}
