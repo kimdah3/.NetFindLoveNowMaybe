014d84269f8c7f333a53d476659d030908fac0a9
Christoffer Sibring <sibring92@gmail.com>
2016-01-08 15:02:44 +0100

    Fixat så man kan ladda upp bilder

---
+++ FindLoveNowMaybe/FindLoveNowMaybe/Controllers/PictureController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Repositories;
+
+namespace FindLoveNowMaybe.Controllers
+{
+    [Authorize]
+    public class PictureController : Controller
+    {
+        [HttpPost]
+        public ActionResult UserPicture(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var filename = Path.GetFileName(file.FileName);
+                var pathname = Path.Combine(Server.MapPath("~/Content/Images"), filename); //Saves the picture in Images
+                file.SaveAs(pathname);
+
+
+                PictureRepository.UploadPicture(User.Identity.Name, filename);
+
+
+            }
+            return RedirectToAction("Profile", "User", new { username = User.Identity.Name });
+        }
+    }
+}
---
+++ FindLoveNowMaybe/Repositories/PictureRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class PictureRepository
+    {
+        public static void UploadPicture(string username, string picture)
+        {
+            using (var db = new FindLoveDbEntities())
+            {
+                User userPicture = db.User.FirstOrDefault(x => x.UserName == username);
+                userPicture.Picture = picture;
+                db.SaveChanges();
+            }
+        }
+    }
+}
