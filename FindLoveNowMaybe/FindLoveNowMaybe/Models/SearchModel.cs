ccaef76aacd826361cb7c108c87f05b8aa6540bf
Christoffer Sibring <sibring92@gmail.com>
2016-01-10 17:40:54 +0100

    Fixat messages och category repository

--- FindLoveNowMaybe/FindLoveNowMaybe/Models/SearchModel.cs
+++ FindLoveNowMaybe/FindLoveNowMaybe/Models/SearchModel.cs
@@ -7,7 +7,7 @@ namespace FindLoveNowMaybe.Controllers
     public class SearchModel
     {
         [Display(Name = "Input first name")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Search field can't be empty!")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Empty search field will return all users!")]
         public string SearchText { get; set; }
         public List<User> FoundUsers { get; set; }
     }
---
+++ FindLoveNowMaybe/Repositories/CategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class CategoryRepository
+    {
+        public List<Category> ReturnAllFriendsWithCategory(string userName)
+        {
+            int userId;
+            var returnList = new List<Category>();
+            using (var userRepo = new UserRepository())
+            {
+                userId = userRepo.GetUserByUserName(userName).Id;
+            }
+            using (var db = new FindLoveDbEntities())
+            {
+                var result = from r in db.Category
+                             where r.ActiveUserId == userId
+                             select r;
+                foreach (var item in result)
+                {
+                    returnList.Add(item);
+                }
+            }
+            return returnList;
+        }
+        public bool CheckIfFriendIsCategorised(string ActiveUserUsername, string FriendUserName)
+        {
+            int ActiveUserId;
+            int FriendId;
+            using (var userRep = new UserRepository())
+            {
+                ActiveUserId = userRep.GetUserByUserName(ActiveUserUsername).Id;
+                FriendId = userRep.GetUserByUserName(FriendUserName).Id;
+            }
+            using (var db = new FindLoveDbEntities())
+            {
+                var p = db.Category.FirstOrDefault(x => x.ActiveUserId == ActiveUserId);
+                return p == null;
+            }
+        }
+    }
+}
