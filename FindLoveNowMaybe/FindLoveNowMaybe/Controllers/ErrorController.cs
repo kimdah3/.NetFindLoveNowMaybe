298841b4fc622e402bf7a4b1e2590179d1f206f1
LinusIvarsson <ivarsson.linus@gmail.com>
2016-01-11 11:48:50 +0100

    ErrorController för HTTP Errors. Uppdaterat textfält för vän-kategori.

---
+++ FindLoveNowMaybe/FindLoveNowMaybe/Controllers/ErrorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindLoveNowMaybe.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult HttpError()
+        {
+            return View();
+        }
+
+        public ActionResult PageNotFound()
+        {
+            return RedirectToAction("HttpError");
+        }
+
+        public ActionResult ServerIssues()
+        {
+            return RedirectToAction("HttpError");
+        }
+
+        public ActionResult GeneralIssue()
+        {
+            return RedirectToAction("HttpError");
+        }
+    }
+}
--- FindLoveNowMaybe/FindLoveNowMaybe/Global.asax.cs
+++ FindLoveNowMaybe/FindLoveNowMaybe/Global.asax.cs
@@ -20,5 +20,32 @@ namespace FindLoveNowMaybe
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Response.Clear();
+
+            HttpException httpException = exception as HttpException;
+            string action;
+            if (httpException != null)
+            {
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        action = "PageNotFound";
+                        break;
+                    case 500:
+                        action = "ServerIssues";
+                        break;
+                    default:
+                        action = "GeneralIssue";
+                        break;
+                }
+                Server.ClearError();
+                Response.Redirect(String.Format("~/Error/{0}/?exceptionMessage={1}", action, exception.Message));
+            }
+        }
+
     }
 }
